f13fbd9c369dfc75fa7d7c143cc0296b410e112e
EinHerrSchmidt <matte24694@gmail.com>
2022-05-02 20:56:40 +0200

    LBBD planner.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
+import logging
+from pyomo.util.infeasible import log_infeasible_constraints
 from data_maker import DataDescriptor, DataMaker, TruncatedNormalParameters
-from planners import SimpleOrderingPlanner, SinglePhaseStartingMinutePlanner, StartingMinutePlanner, TwoPhaseStartingMinutePlanner
+from planners import Planner, TwoPhaseStartingMinutePlanner
 if __name__ == '__main__':
 
-    planner = SinglePhaseStartingMinutePlanner(timeLimit=900, solver="cplex")
+    # planner = Planner(timeLimit=900, solver="cbc")
+    planner = TwoPhaseStartingMinutePlanner(timeLimit=900, solver="cbc")
 
     dataDescriptor = DataDescriptor()
-    dataDescriptor.patients = 40
+
+    # complicated instance
+    # dataDescriptor.patients = 60
+    # dataDescriptor.days = 5
+    # dataDescriptor.anesthetists = 2
+    # dataDescriptor.covidFrequence = 0.8
+    # dataDescriptor.anesthesiaFrequence = 0.7
+    # dataDescriptor.specialtyBalance = 0.17
+    # dataDescriptor.operatingDayDuration = 180
+    # dataDescriptor.anesthesiaTime = 180
+
+    dataDescriptor.patients = 80
     dataDescriptor.days = 5
     dataDescriptor.anesthetists = 2
-    dataDescriptor.covidFrequence = 0.8
-    dataDescriptor.anesthesiaFrequence = 0.3
+    dataDescriptor.covidFrequence = 0.4
+    dataDescriptor.anesthesiaFrequence = 0.5
     dataDescriptor.specialtyBalance = 0.17
     dataDescriptor.operatingDayDuration = 240
     dataDescriptor.anesthesiaTime = 240
@@ -33,3 +47,5 @@ if __name__ == '__main__':
     print("Possible solution, for each day and for each room:\n")
     planner.print_solution()
     planner.plot_graph()
+    # log_infeasible_constraints(planner.modelInstancePhaseTwo, log_expression=True, log_variables=True)
+    # logging.basicConfig(filename='unfeasible.log', encoding='utf-8', level=logging.INFO)
