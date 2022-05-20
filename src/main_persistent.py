42457eebe5e9aa5ea6ffb202e0c322d5b8e73c6b
EinHerrSchmidt <matte24694@gmail.com>
2022-05-20 21:03:59 +0200

    Just to keep aligned for weekend.

--- src/main_persistent.py
+++ src/main_persistent.py
@@ -2,15 +2,15 @@ import logging
 import time
 from pyomo.util.infeasible import log_infeasible_constraints
 from data_maker import DataDescriptor, DataMaker, TruncatedNormalParameters
-from LBBDPlanner import Planner
+from fast_complete_heuristic import Planner
 from utils import SolutionVisualizer
 if __name__ == '__main__':
 
-    planner = Planner(timeLimit=30, solver="cplex")
+    planner = Planner(timeLimit=900, solver="cplex")
     dataDescriptor = DataDescriptor()
 
     # complicated instance
-    dataDescriptor.patients = 60
+    dataDescriptor.patients = 120
     dataDescriptor.days = 5
     dataDescriptor.anesthetists = 2
     dataDescriptor.covidFrequence = 0.8
@@ -43,16 +43,15 @@ if __name__ == '__main__':
     print(dataDescriptor)
     t = time.time()
     # print("\nPatients to be operated:\n")
-    # dataMaker.print_data(dataDictionary)
+    dataMaker.print_data(dataDictionary)
     runInfo = planner.solve_model(dataDictionary)
     elapsed = (time.time() - t)
 
     logging.basicConfig(filename='times.log',
                         encoding='utf-8', level=logging.INFO)
-    logging.info(
-        "MP_building_time\tSP_overall_building_time\tTotal_run_time\tSolver_time\tStatus_OK\tObjective_Function_Value\tIterations")
+    logging.info("MP_building_time\tSP_building_time\tTotal_run_time\tSolver_time\tStatus_OK\tObjective_Function_Value\tIterations")
     logging.info(str(runInfo["MPBuildingTime"]) + "\t"
-                 + str(runInfo["overallSPBuildingTime"]) + "\t"
+                 + str(runInfo["SPBuildingTime"]) + "\t"
                  + str(round(elapsed, 2)) + "\t"
                  + str(runInfo["solutionTime"]) + "\t"
                  + str(runInfo["statusOk"]) + "\t"
