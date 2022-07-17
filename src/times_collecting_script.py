647ccf82aa1904c4b1599a5c01aa692d4de54b4d
EinHerrSchmidt <matte24694@gmail.com>
2022-07-17 23:50:12 +0200

    Updates and fixes.

--- src/times_collecting_script.py
+++ src/times_collecting_script.py
@@ -2,8 +2,12 @@ import os
 
 if __name__ == '__main__':
     # "slow" complete heuristic
-    os.system("python main_SCE.py False")
+    # os.system("python main_SCE.py False")
     # "fast" complete heuristic
-    os.system("python main_FCE.py False")
+    # os.system("python main_FCE.py False")
     # vanilla
-    os.system("python main_vanilla.py")
+    # os.system("python main_vanilla.py")
+    # LBBD
+    os.system("python main_LBBD.py False 10")
+    # 3-phase LBBD
+    os.system("python main_LBBD.py False 10")
